f3f5d708089a678545107a61c4c3a68b8dfe2d94
Emmanuel Rudelle <erudelle@meetphone.com>
2016-12-22 10:34:21 +0100

    Add map maker

--- IndexLocations.js
+++ IndexLocations.js
@@ -8,17 +8,33 @@ import { styles } from './StyleSheet'
 class IndexLocations extends Component {
 
   render(){
+    var markers = [
+      {
+        latitude: 48.866667,
+        longitude: 2.333333,
+        title: 'Foo Place',
+        subtitle: '1234 Foo Drive'
+      }
+    ];
+
     return(
       <View style={styles.mapContainer}>
         <MapView
           style={styles.map}
            initialRegion={{
-             latitude: 37.78825,
-             longitude: -122.4324,
-             latitudeDelta: 0.0922,
+             latitude: 46.8167,
+             longitude: 1.7,
+             latitudeDelta: 0.1922,
              longitudeDelta: 0.0421,
            }}
-         />
+         >
+      <MapView.Marker
+       coordinate={{latitude: 46.8167,
+       longitude: 1.7}}
+       title={"title"}
+       description={"description"}
+        />
+     </MapView>
      </View>
 
     )
